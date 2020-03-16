c1fbda7ff928f8acea4d310189116f65fbb6994f
Renata Terra <renatamaria.terra@gmail.com>
2020-03-16 13:59:03 -0300

    Exercícios - Lista 3

--- Lista3/src/br/edu/fatecfranca/ads/ex3/TestaRodoviaria.java
+++ Lista3/src/br/edu/fatecfranca/ads/ex3/TestaRodoviaria.java
@@ -2,10 +2,32 @@ package br.edu.fatecfranca.ads.ex3;
 
 import javax.swing.JOptionPane;
 
-public class Teste {
+public class TestaRodoviaria {
 
     public static void main(String[] args) {
+        Passageiro pas1 = new Passageiro("490963001", "Carla");
+        Passageiro pas2 = new Passageiro("490963008", "Romeu");
+        Passageiro pas3 = new Passageiro("490963075", "João");
         
+        Onibus onibus1 = new Onibus();
+        onibus1.setNumero(1);
+        onibus1.setPlaca("HBS1234");
+        onibus1.addPassageiro(pas1);
+        
+        Onibus onibus2 = new Onibus();
+        onibus2.setNumero(2);
+        onibus2.setPlaca("SGR9101");
+        onibus2.addPassageiro(pas2);
+        onibus2.addPassageiro(pas3);
+        
+        Rodoviaria rod = new Rodoviaria();
+        rod.setNome("Tietê");
+        rod.setCidade("São Paulo");
+        rod.addOnibus(onibus1);
+        rod.addOnibus(onibus2);
+
+        JOptionPane.showMessageDialog(null, onibus1.toString());
+        JOptionPane.showMessageDialog(null, onibus2.toString());
+        JOptionPane.showMessageDialog(null, rod.toString());
     }
-    
 }
